3423924d9ed9339717391e3dfe8eb6f93a16e931
vagner santos <vagner131417@gmail.com>
2022-12-16 16:14:56 -0300

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,7 @@ module.exports = {
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
     },
-
+    "editor.codeActionsOnSave": {
+        "source.fixAll.eslint": true,
+    },
 };
