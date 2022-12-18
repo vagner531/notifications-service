560796553c4f6067773d7bc6cbd02d965c6be038
vagner santos <vagner131417@gmail.com>
2022-12-18 16:38:22 -0300

    create entities

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,28 +1,53 @@
 export class NotificationProps {
+  recipientId: string;
   content: string;
   category: string;
+  readAt?: Date | null;
+  createdAt: Date;
 }
 
 export class Notification {
   private props: NotificationProps;
 
-  constructor() {
-    this.content = '';
+  constructor(props: NotificationProps) {
+    this.props = props;
   }
 
-  public set content(content: string) {
-    if (content.length < 5) {
-      throw new Error();
-    }
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId
+  }
 
+  public get recipientId(): string {
+    return this.props.recipientId;
+  } 
+
+  public set content(content: string) {
     this.props.content = content
   }
 
   public get content(): string {
     return this.props.content;
-  } 
+  }
+
+  public set category(category: string) {
+    this.props.category = category
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
 }
 
-const notification = new Notification()
-
-notification.content = 'kshdskjdsjd'
